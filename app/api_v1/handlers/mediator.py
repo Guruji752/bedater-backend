4682d80641477ed0b3af0d4ba1448418a3ae7df9
yogesh <yogesh1999@tradeindia.com>
2025-04-02 23:30:50 +0530

    changes

--- app/api_v1/handlers/mediator.py
+++ app/api_v1/handlers/mediator.py
@@ -6,9 +6,9 @@ from app.services.MediatorServices import MediatorServices
 from app.models.auth.UserMaster import UserMaster
 mediator_router = APIRouter()
 @mediator_router.get("/screen/details",summary="This api will provide topic list to meditor")
-async def debateTopics(debate_id:int,virtual_id,db:Session=Depends(get_transaction_session),user:UserMaster=Depends(get_current_user)):
+async def debateTopics(debate_id:int,db:Session=Depends(get_transaction_session),user:UserMaster=Depends(get_current_user)):
 	try:
-		return await MediatorServices.screenDetails(debate_id,virtual_id,db)
+		return await MediatorServices.screenDetails(debate_id,db)
 	except Exception as e:
 		raise HTTPException(
 			status_code=status.HTTP_400_BAD_REQUEST,
